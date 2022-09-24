1918de25d6ab3bfda0a0184bd15eb1c877db9a93
joewallacerose <2565536r@student.gla.ac.uk>
2022-09-24 14:14:09 +0100

    installed TypeScript and typed Modal file

--- src/Modal.tsx
+++ src/Modal.tsx
@@ -1,24 +1,38 @@
-import { useEffect, useRef } from "react";
+import { FunctionComponent, MutableRefObject, useEffect, useRef } from "react";
 import { createPortal } from "react-dom";
 
-const Modal = ({ children }) => {
+const Modal: FunctionComponent = ({ children }) => {
   // useRef means that an object is passed by reference across
   // all renders. This means that each time you invoke this
   // argument you are refering to the exact same object, not just
   // an equivalent one (===)
-  const elRef = useRef(null);
+
+  // Here we are expressing that elRef will always either by a HTMLDivElement
+  // or nothing, otherwise we'll get an error.
+  const elRef: MutableRefObject<HTMLDivElement | null> = useRef(null);
   if (!elRef.current) {
     elRef.current = document.createElement("div");
   }
 
   useEffect(() => {
     const modalRoot = document.getElementById("modal");
+    // We can't call appendChild on modalRoot unless we assure TypeScript that
+    // it won't be null.
+    if (!modalRoot || !elRef.current) {
+      return;
+    }
     modalRoot.appendChild(elRef.current);
 
     // This function is invoked every time this Modal goes away.
     // Used for cleanup. Every function that you return from
     // your effect should be involved in its
-    return () => modalRoot.removeChild(elRef.current);
+
+    // We have to repeat the same null check as we did earlier.
+    return () => {
+      if (elRef.current) {
+        modalRoot.removeChild(elRef.current);
+      }
+    };
   }, []);
 
   return createPortal(<div>{children}</div>, elRef.current);
